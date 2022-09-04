cc9d66b96885ddf9563cfcc233e574e0b119a5c7
sanyuchilas <alex-keyer@mail.ru>
2022-09-04 15:34:12 +0300

    Solve task

--- 8/app.js
+++ 8/app.js
@@ -1,4 +1,5 @@
 'use strict';
+import addToCart from './basket.js'
 
 let fitlerPopup = document.querySelector('.filterPopup');
 let fitlerLabel = document.querySelector('.filterLabel');
@@ -28,3 +29,24 @@ let filterSizeWrap = document.querySelector('.filterSizeWrap');
 filterSizeWrap.addEventListener('click', function() {
     filterSizes.classList.toggle('hidden');
 });
+
+document.querySelector('.featuredItems').addEventListener('click', event => {
+    if (
+        event.target.parentElement.textContent.trim() === 'Add to cart'
+        || event.target.classList.contains('featuredImgDark')
+    ) {
+        return
+    }
+    
+    const getProductId = element => {
+        if (element.dataset.id) {
+            return element.dataset.id
+        } 
+      
+        return getProductId(element.parentElement)
+    }
+    
+    const productId = getProductId(event.target)
+
+    addToCart(productId)
+})
--- 8/basket.js
+++ 8/basket.js
@@ -1,1 +1,50 @@
-console.log(1)
+const productsMock = [
+  { id: 1, name: 'Example 1', price: 1.12, count: 0 },
+  { id: 2, name: 'Example 2', price: 52.12, count: 0 },
+  { id: 3, name: 'Example 3', price: 32, count: 0 },
+]
+const productIds = new Set()
+
+const basket = document.querySelector('.basket')
+const basketTotalValue = document.querySelector('.basketTotalValue')
+const basketProductsList = document.querySelector('.basketProductsList')
+const cartIconWrap = document.querySelector('.cartIconWrap')
+const createProductMarkup = ({id, name, count, price}) => 
+  `
+    <div class="basketProduct row">
+      <div>${name}</div>
+      <div>${count} шт.</div>
+      <div>$${price.toFixed(2)}</div>
+      <div>$${(count * price).toFixed(2)}</div>
+    </div>
+  `
+
+export default function addToCart(productId) {
+  if (!productId) return
+  
+  let totalPrice = 0
+  
+  basketProductsList.innerHTML = ''
+
+  productIds.add(productId)
+
+  cartIconWrap.querySelector('span').textContent++
+
+  productIds.forEach(id => {
+    const product = productsMock[id - 1]
+
+    if (id === productId) {
+      product.count++
+    }
+
+    basketProductsList
+      .insertAdjacentHTML('beforeend', createProductMarkup(product))
+   
+    totalPrice += product.count * product.price
+  })
+
+  basketTotalValue.textContent = 
+    'Товаров в корзине на сумму: $' + totalPrice.toFixed(2)
+}
+
+cartIconWrap.addEventListener('click', event => basket.classList.toggle('hidden'))
